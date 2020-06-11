94ddc2f4de679c7275a5dc89a0a505ab5e2abc9b
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-06-11 14:27:44 +0000

    Se actualiza estilo del formulario

--- src/js/component/registerForm.js
+++ src/js/component/registerForm.js
@@ -7,92 +7,59 @@ export const RegisterForm = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<form className="container">
-			<div className="form-row">
-				<div className="col">
-					<h2 className="text-center titleForm">
-						Formulario <p>de Registro</p>
-					</h2>
+		<div>
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-12 col-md-8 col-lg-8 col-xl-6">
+						<div className="row">
+							<div className="col text-center title">
+								<h1 className="titleForm">Formulario de Registro</h1>
+							</div>
+						</div>
+						<div className="row align-items-center mt-4">
+							<div className="col">
+								<input type="password" className="form-control" placeholder="CIF o NIF" />
+							</div>
+							<div className="col">
+								<input type="password" className="form-control" placeholder="Nombre de la empresa" />
+							</div>
+						</div>
+						<div className="row align-items-center mt-4">
+							<div className="col">
+								<input type="email" className="form-control" placeholder="Email" />
+							</div>
+						</div>
+						<div className="row align-items-center mt-4">
+							<div className="col">
+								<input type="text" className="form-control" placeholder="Company Name" />
+							</div>
+						</div>
+						<div className="row align-items-center mt-4">
+							<div className="col">
+								<input type="text" className="form-control" placeholder="Postal Address" />
+							</div>
+						</div>
+						<div className="row align-items-center mt-4">
+							<div className="col">
+								<input type="password" className="form-control" placeholder="Contraseña" />
+							</div>
+							<div className="col">
+								<input type="password" className="form-control" placeholder="Confirma contraseña" />
+							</div>
+						</div>
+						<div className="row justify-content-start mt-4">
+							<div className="col">
+								<button className="btn-lg buttonSubmit">Submit</button>
+							</div>
+							<Link to="/">
+								<span className="btn btn-primary btn-lg" href="#" role="button">
+									Back home
+								</span>
+							</Link>
+						</div>
+					</div>
 				</div>
 			</div>
-			<div className="form-group row">
-				<div className="col-6">
-					<label className="labelRegister" htmlFor="exampleInputPassword1">
-						CIF o NIF
-					</label>
-					<input
-						type="text"
-						className="form-control input-lg"
-						id="exampleInputPassword1"
-						placeholder="Escribe el CIF"
-					/>
-				</div>
-				<div className="col-6">
-					<label className="labelRegister" htmlFor="exampleInputEmail1">
-						NOMBRE DE LA EMPRESA
-					</label>
-					<input
-						type="text"
-						className="form-control input-lg"
-						id="exampleInputEmail1"
-						aria-describedby="emailHelp"
-						placeholder="Escribe el nombre de la empresa"
-					/>
-				</div>
-			</div>
-			<div className="form-group">
-				<label className="labelRegister" htmlFor="exampleInputPassword1">
-					CORREO ELECTRÓNICO
-				</label>
-				<input
-					type="password"
-					className="form-control"
-					id="exampleInputPassword1"
-					placeholder="Escribe el Correo Electrónico"
-				/>
-			</div>
-			<div className="form-group">
-				<label className="labelRegister" htmlFor="exampleInputPassword1">
-					NOMBRE DE LA MARCA
-				</label>
-				<input
-					type="password"
-					className="form-control input-lg"
-					id="exampleInputPassword1"
-					placeholder="Escribe el Nombre de la Marca"
-				/>
-			</div>
-			<div className="form-group">
-				<label className="labelRegister" htmlFor="exampleInputPassword1">
-					CONTRASEÑA
-				</label>
-				<input
-					type="password"
-					className="form-control input-lg"
-					id="exampleInputPassword1"
-					placeholder="Escribe la contraseña"
-				/>
-			</div>
-			<div className="form-group">
-				<label className="labelRegister" htmlFor="exampleInputPassword1">
-					TELÉFONO
-				</label>
-				<input
-					type="password"
-					className="form-control input-lg"
-					id="exampleInputPassword1"
-					placeholder="Escribe el CIF"
-				/>
-			</div>
-			<div className="form-check">
-				<input type="checkbox" className="form-check-input" id="exampleCheck1" />
-				<label className="form-check-label" htmlFor="exampleCheck1">
-					Check me out
-				</label>
-			</div>
-			<button type="submit" className="btn btn-primary">
-				Submit
-			</button>
-		</form>
+		</div>
 	);
 };
