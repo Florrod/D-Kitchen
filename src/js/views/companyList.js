b5bb86fd7242b3148dcb6de8e9019f8a31bf2ab1
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-06-17 11:12:15 +0000

    Se añade título al CompanyList

--- src/js/views/companyList.js
+++ src/js/views/companyList.js
@@ -7,22 +7,15 @@ import { RegisterForm } from "../component/registerForm.js";
 
 export const CompanyList = () => {
 	const [state, setState] = useState({
-		showModal: false
 	});
 
 	return (
 		<div className="container">
 			<div>
-				<p className="text-right my-3">
-					<Link className="button" to="/registerForm">
-						Añadir nueva empresa
-					</Link>
-				</p>
-				<div id="contacts" className="panel-collapse collapse show" aria-expanded="true">
-					<ul className="list-group pull-down" id="contact-list">
-						<CompanyCard onDelete={() => setState({ showModal: true })} />
-					</ul>
-				</div>
+				<h1 className="text-center mt-0">Directorio de empresas</h1>
+				<ul className="list-group pull-down" id="contact-list">
+					<CompanyCard />
+				</ul>
 			</div>
 		</div>
 	);
