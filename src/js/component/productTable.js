1cca2077bb51a1c993304e6a0f00b8e19ba90753
Flor Rodriguez <florrod23@gmail.com>
2020-08-06 19:51:58 +0000

    Show top products table

--- src/js/component/productTable.js
+++ src/js/component/productTable.js
@@ -4,61 +4,34 @@ import PropTypes from "prop-types";
 import { Link, useRouteMatch, useParams, useHistory } from "react-router-dom";
 import "../../styles/salesTable.scss";
 
-const ENDPOINT = "https://3000-d3f07519-d893-4493-bc4a-c846bc31aa31.ws-eu01.gitpod.io";
+const ENDPOINT = "https://3000-d3f07519-d893-4493-bc4a-c846bc31aa31.ws-eu01.gitpod.io/";
 
 export const ProductTable = props => {
-	const [state, setState] = useState({
-		//initialize state here
-	});
-
-	const [products, setProducts] = useState({});
 	const [platforms, setPlatforms] = useState([]);
-
-	useEffect(
-		() => {
-			platforms.forEach(p => {
-				getTopProducts(p.id).then(data_from_products => {
-					setProducts({
-						...products,
-						[p.id]: data_from_products
-					});
-				});
-			});
-		},
-		[platforms]
-	); //Cada vez que se modifique products se producirá este efecto secundario
-
-	const getPlatforms = platform_id => {
-		let access_token = localStorage.getItem("access_token");
-		return fetch(`${ENDPOINT}/integration/platform`, {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${access_token}`
-			}
-		})
-			.then(platform_json => {
-				platforms = platform_json;
-				return platforms;
-			})
-			.then(platforms => {
-				setPlatforms(platforms);
-				let products = {};
-				platforms.forEach(p => (products[p.id] = []));
-				setProducts(products);
-			});
-	};
 
 	const getTopProducts = platform_id => {
 		let access_token = localStorage.getItem("access_token");
-		return fetch(`${ENDPOINT}/top-product/${platform_id}`, {
+		return fetch(`${ENDPOINT}/top-products/${platform_id}`, {
 			method: "GET",
 			headers: {
 				"Content-Type": "application/json",
-				Authorization: `Bearer ${access_token}`
+				Authorization: `Bearer ${access_token}`,
+				"Access-Control-Allow-Origin": "*"
 			}
-		}).then(res => res.json());
+		})
+			.then(res => res.json())
+			.then(platforms => {
+				setPlatforms(platforms);
+			});
 	};
+
+	useEffect(() => {
+		getTopProducts(1);
+	}, []);
+
+	if (platforms == null) return "";
+
+	if (platforms[0] == null) return "";
 
 	return (
 		<div className="table-responsive-sm">
@@ -66,17 +39,28 @@ export const ProductTable = props => {
 				<div className="col-4">
 					<div className="row">
 						<p>Ranking</p>
-						{platforms.map((index, plat) => (
-							<div className="col-12" key={index} products={products[plat.id]} />
-						))}
+						{platforms
+							? platforms.map((plat, index) => (
+									<div className="col-12" key={plat.id}>
+										{plat.name}
+									</div>
+							  ))
+							: ""}
 					</div>
 					<div className="row">
-						{products.map((index, prod) => (
-							<div className="col-12" key={index}>
-								{" "}
-								{prod.name}
-							</div>
-						))}
+						{platforms
+							? platforms.map((plat, index) => (
+									<div key={plat.id} className="platform">
+										{plat.top_products.map((product, index) => (
+											<div className="col-12" key={product.name}>
+												{" "}
+												{product.name}
+											</div>
+										))}
+									</div>
+							  ))
+							: ""}
+						;
 					</div>
 				</div>
 			</div>
